dcbe992e3d65aa7480cbfb407eb707ca7180a036
Shafi Masoumi <masoumi2017@icloud.com>
2019-11-13 23:01:01 -0800

    step 4 done

--- src/App.js
+++ src/App.js
@@ -18,26 +18,18 @@ function App() {
 	};
 
 	return (
+		<ProductContext.Provider value={{ products, addItem }}>
 		<div className="App">
 			<Navigation cart={cart} />
-
 			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
+			<Route exact path="/" component={Products} />
 
 			<Route
 				path="/cart"
 				render={() => <ShoppingCart cart={cart} />}
 			/>
 		</div>
+		</ProductContext.Provider>
 	);
 }
 
